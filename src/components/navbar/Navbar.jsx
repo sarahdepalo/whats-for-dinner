d56c6b6a09f58d0027109f808623f4c2d4f9287b
Sarah dePalo <sarahdepalo99@gmail.com>
2021-09-01 10:20:32 -0400

    initial commit

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import logo from './logo.png';
+
+const Navbar = () => {
+    return (
+        <>
+        <img src={logo} alt="What's For Dinner Logo"/>
+        <a href="">Github</a>
+        </>
+    )
+}
+
+export default Navbar;
