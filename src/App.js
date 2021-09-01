ce59ec34057432c842773bbf038d14d568357907
Sarah dePalo <sarahdepalo99@gmail.com>
2021-09-01 13:03:40 -0400

    created introContainer and finished styles for it

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-
+import { BrowserRouter as Router} from 'react-router-dom';
 import Navbar from './components/navbar/Navbar';
+import IntroContainer from './components/introContainer/IntroContainer';
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
-      <div className="wrapper">
-       <h1>Never Think About Choosing Dinner Again</h1>
-       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vestibulum est vitae finibus rhoncus. Mauris ut tincidunt arcu. Quisque massa eros, ultricies vitae lobortis ac, ultrices a justo. Nulla viverra sed mi non hendrerit. Interdum et malesuada fames ac ante ipsum primis in faucibus. Duis luctus mi convallis metus tincidunt mattis. Etiam pellentesque vel leo quis tincidunt.</p>
-      </div>
+      <Router>
+        <Navbar/>
+        <div className="wrapper">
+          <IntroContainer/>
+        </div>
+      </Router>
     </div>
   );
 }
---
+++ src/components/introContainer/IntroContainer.jsx
@@ -0,0 +1,26 @@
+import { Link } from 'react-router-dom';
+import burgerAndFries from './burgerAndFries.png';
+import "./introContainer.scss";
+
+const IntroContainer = () => {
+  return (
+    <>
+      <div className="row">
+        <div className="col">
+          <h1>Never Think About Choosing Dinner Again</h1>
+          <div className="paragraphBox">
+          <p>
+          Tired of being asked, "What's for dinner"? So are we. It's time to put an end to that question. With our dinner generator, you can easily select a meal by protein or cuisine. Don't like what we gave you? Just hit the Let's Eat button for a new option. 
+          </p>
+            <Link to="#get-dinner" className="primaryBtn">GET STARTED</Link>
+          </div>
+        </div>
+        <div className="col">
+            <img src={burgerAndFries} alt="burger and fries"/>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default IntroContainer;
